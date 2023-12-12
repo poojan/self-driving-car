d612cdc55887da8aaca38f5bbcd698816897ba23
Poojan Shrestha <poojans@gmail.com>
2023-12-12 12:02:05 -0700

    Flip left/right controls when car backs up.

--- src/app/car.ts
+++ src/app/car.ts
@@ -50,11 +50,15 @@ export class Car {
         if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-        if (this.controls.left) {
-            this.angle += 0.03;
-        }
-        if (this.controls.right) {
-            this.angle -= 0.03;
+        if (this.speed !== 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
         }
 
         this.x -= Math.sin(this.angle) * this.speed;
